c27db78c76422fe2f13b001ceb67b67c0aa921cd
laoqu <quguiyou1076@qq.com>
2022-11-27 15:43:48 +0800

    An Actual Compiler

--- main.c
+++ main.c
@@ -33,7 +33,8 @@ static void scanfile()
 }
 
 // Print out a usage if started incorrectly
-static void usage(char *prog) {
+static void usage(char *prog)
+{
     fprintf(stderr, "Usage: %s infile\n", prog);
     exit(1);
 }
@@ -41,18 +42,31 @@ static void usage(char *prog) {
 void main(int argc, char *argv[])
 {
     struct ASTnode *n;
-    if(argc != 2) {
+    if (argc != 2)
+    {
         usage(argv[0]);
     }
     init();
 
-    if((Infile = fopen(argv[1], "r")) == NULL) {
+    // Open up the input file
+    if ((Infile = fopen(argv[1], "r")) == NULL)
+    {
         fprintf(stderr, "Unable to open %s: %s\n", argv[1], strerror(errno));
         exit(0);
+    }
+
+    // Create the output file
+    if ((Outfile = fopen("out.s", "w")) == NULL)
+    {
+        fprintf(stderr, "Unable to create out.s: %s\n", strerror(errno));
+        exit(1);
     }
 
     scan(&Token);
     n = binexpr(0);
     printf("%d\n", interpretAST(n));
+    generatecode(n);
+
+    fclose(Outfile);
     exit(0);
 }
